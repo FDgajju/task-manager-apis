5d70c067604347fca88de3c3f1305d5b37348678
Gajendra sahu <92583148+FDgajju@users.noreply.github.com>
2025-06-22 03:45:58 +0530

    update; document schema add created and updated at

--- src/components/documents/schema.ts
+++ src/components/documents/schema.ts
@@ -3,10 +3,15 @@ import type { ObjectId } from "mongodb";
 
 export const DocumentCreateSchema = Type.Object({
   _id: Type.Optional(Type.Unsafe<ObjectId>({ type: "string" })), // Accepts MongoDB ObjectId as string
+
+  originalname: Type.String(),
   name: Type.String(),
   path: Type.String(),
   type: Type.Optional(Type.String()),
   url: Type.Optional(Type.String()),
+
+  createdAt: Type.Optional(Type.String()),
+  updatedAt: Type.Optional(Type.String()),
 });
 
 export const DocumentSchema = Type.Object({
@@ -17,6 +22,9 @@ export const DocumentSchema = Type.Object({
   path: Type.String(),
   type: Type.Optional(Type.String()),
   url: Type.Optional(Type.String()),
+
+  createdAt: Type.Optional(Type.String()),
+  updatedAt: Type.Optional(Type.String()),
 });
 
 export type DocumentT = Static<typeof DocumentSchema>;
