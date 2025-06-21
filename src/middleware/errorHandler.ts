140bc55db5a62247ffed06110a3bfce0f348dc8c
Gajendra sahu <92583148+FDgajju@users.noreply.github.com>
2025-06-21 15:37:50 +0530

    formation changes

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -3,44 +3,45 @@ import { AppError } from "../components/utils/AppError.ts";
 import { HTTP_STATUS } from "../constants/HTTP_STATUS.ts";
 
 const sendValidationError = (err: FastifyError, reply: FastifyReply) => {
-	return reply.status(HTTP_STATUS.BAD_REQUEST).send({
-		status: false,
-		error: err.message || "Invalid request data",
-		statusCode: HTTP_STATUS.BAD_REQUEST,
-		data: null,
-	});
+  return reply.status(HTTP_STATUS.BAD_REQUEST).send({
+    status: false,
+    error: err.message || "Invalid request data",
+    statusCode: HTTP_STATUS.BAD_REQUEST,
+    data: null,
+  });
 };
 
 const sendInternalServerErrors = (err: FastifyError, reply: FastifyReply) => {
-	return reply.status(500).send({
-		status: false,
-		error: err.message || "Internal Server Error",
-		statusCode: 500,
-		data: null,
-	});
+  return reply.status(500).send({
+    status: false,
+    error: err.message || "Internal Server Error",
+    statusCode: 500,
+    data: null,
+  });
 };
 
 export const errorHandler = async (
-	error: FastifyError,
-	req: FastifyRequest,
-	reply: FastifyReply,
+  error: FastifyError,
+  req: FastifyRequest,
+  reply: FastifyReply
 ) => {
-	if (error instanceof AppError) {
-		req.log.error(error, "Request error 💥");
 
-		reply.status(error.statusCode).send({
-			status: error.status,
-			error: error.message,
-			statusCode: error.statusCode,
-			details: error.details,
-			data: null,
-		});
-	} else {
-		req.log.error({ err: error, req }, "Request error 💥");
+  if (error instanceof AppError) {
+    req.log.error(error, "Request error 💥");
 
-		if (error.code === "FST_ERR_VALIDATION")
-			return sendValidationError(error, reply);
+    reply.status(error.statusCode).send({
+      status: error.status,
+      error: error.message,
+      statusCode: error.statusCode,
+      details: error.details,
+      data: null,
+    });
+  } else {
+    req.log.error({ err: error, req }, "Request error 💥");
 
-		return sendInternalServerErrors(error, reply);
-	}
+    if (error.code === "FST_ERR_VALIDATION")
+      return sendValidationError(error, reply);
+
+    return sendInternalServerErrors(error, reply);
+  }
 };
