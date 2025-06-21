0fbc525124ad229a70c16cc4af856a62ded9021a
Gajendra sahu <92583148+FDgajju@users.noreply.github.com>
2025-06-21 15:38:10 +0530

    Add object id type in global type

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,2 +1,5 @@
+import type { mongodb } from "@fastify/mongodb";
+
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
 export type AnyType = any | any[];
+export type OID = mongodb.ObjectId;
